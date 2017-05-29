b33a8b7f6ee66ce5a432400a427e7952c2575aa1
Kolodziejczyk Jakub <k.jakub@lexues.co.jp>
2017-05-29 16:49:18 +0900

    Implemented Tools::CalculateRMSE(), stubbed out estimates with some simple constant offsets from ground truth

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,18 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd squared_errors_sum(4) ;
+  squared_errors_sum.fill(0) ;
+
+  for(int i = 0 ; i < estimations.size() ; ++i)
+  {
+    VectorXd difference = estimations[i] - ground_truth[i] ;
+    VectorXd squared_difference = difference.array() * difference.array() ;
+
+    squared_errors_sum += squared_difference ;
+  }
+
+  VectorXd MSE = squared_errors_sum / estimations.size() ;
+  return MSE.array().sqrt() ;
+
 }
